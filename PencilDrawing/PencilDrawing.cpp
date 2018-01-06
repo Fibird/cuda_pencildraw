3840e45dee9bf0a66bba6d37497f2c585184bbcc
Liu Chaoyang <chaoyanglius@gmail.com>
2018-01-06 11:37:48 +0800

    Make program output right result
    * unify the data type of image matrices
    * simplify some processes

--- PencilDrawing/PencilDrawing.cpp
+++ PencilDrawing/PencilDrawing.cpp
@@ -12,17 +12,19 @@ using namespace cv;
 
 int main(int argc, char** argv)
 {
+	Mat image;
+	int ks = 10;
+	const int dir_num = 8;
+	int data_type = CV_32FC1;
+
 	if (argc != 2)
 	{
 		cout << "No arguments are specified!" << endl;
 		return -1;
 	}
 
-	Mat image;
-	int ks = 10;
-	const int dir_num = 8;
 	image = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
-
+	image.convertTo(image, CV_32F, 1.0 / 255.0);
 	// Get the depth of input image
 	int img_dpt = image.depth();
 	// Get the size info of input image
@@ -30,49 +32,52 @@ int main(int argc, char** argv)
 	img_h = image.rows;
 	img_w = image.cols;
 	Size img_s = image.size();
-
+	
 	// Remove noise
-	medianBlur(image, image, 7);
-
+	medianBlur(image, image, 3);
+	
 	///// Get Image gradient
 	Mat grad_x, grad_y, grad;
+	grad_x = Mat::zeros(Size(img_w, img_h), CV_32FC1);
+	grad_y = Mat::zeros(Size(img_w, img_h), CV_32FC1);
 	// Gradient X
-	Sobel(image, grad_x, img_dpt, 1, 0, 3, 1, 0, BORDER_DEFAULT);
-	convertScaleAbs(grad_x, grad_x);
+	grad_x(Rect(1, 0, img_w - 1, img_h)) =
+		abs(image(Rect(0, 0, img_w - 1, img_h)) - 
+			image(Rect(1, 0, img_w - 1, img_h)));
 	// Gradient Y
-	Sobel(image, grad_y, img_dpt, 0, 1, 3, 1, 0, BORDER_DEFAULT);
-	convertScaleAbs(grad_y, grad_y);
-
-	addWeighted(grad_x, 0.5, grad_y, 0.5, 0, grad);
+	grad_y(Rect(0, 0, img_w, img_h - 1)) = 
+		abs(image(Rect(0, 0, img_w, img_h - 1)) - 
+			image(Rect(0, 1, img_w, img_h - 1)));
+	//addWeighted(grad_x, 0.5, grad_y, 0.5, 0, grad);
+	grad = grad_x + grad_y;
 
 	///// Classification
-	Mat ker_ref = Mat::zeros(ks * 2 + 1, ks * 2 + 1, CV_8U);
+	Mat ker_ref = Mat::zeros(ks * 2 + 1, ks * 2 + 1, CV_32FC1);
 
 	// Convolution kernel with horizontal direction
-	for (int i = 0; i < ks * 2 + 1; i++)
-	{
-		ker_ref.at<char>(ks, i) = 1;
-	}
-	
+	ker_ref(Rect(0, ks, ks * 2 + 1, 1)) = Mat::ones(1, ks * 2 + 1, CV_32FC1);
+
 	// Group gradient magnitudes
 	Mat response[dir_num];
 	Mat ker_real, rot_mat;
+	int rp_dpt = CV_32F;
 	for (int i = 0; i < dir_num; i++)
 	{
-		rot_mat = getRotationMatrix2D(Point2f(ks, ks), (i - 1) * 180 / dir_num, 1.0);
+		rot_mat = getRotationMatrix2D(Point2f((float)ks, (float)ks), 
+			(float)i * 180.0 / (float)dir_num, 1.0);
 		warpAffine(ker_ref, ker_real, rot_mat, ker_ref.size());
-		filter2D(grad, response[i], grad.depth(), ker_real);
+		filter2D(grad, response[i], rp_dpt, ker_real);
 	}
-	
+
 	Mat indices = Mat::zeros(img_s, CV_8U);
 	// Select the indices of maximum value
 	for (unsigned int i = 0; i < img_h * img_w; i++)
 	{
 		char max_index = 0;
-		int max_value = response[0].at<char>(i);
+		float max_value = response[0].at<float>(i);
 		for (int j = 1; j < dir_num; j++)
 		{	
-			int cur_value = response[j].at<char>(i);
+			float cur_value = response[j].at<float>(i);
 			if (max_value < cur_value)
 			{
 				max_index = j;
@@ -81,12 +86,13 @@ int main(int argc, char** argv)
 		}
 		indices.at<char>(i) = max_index;
 	}
-
+	
 	Mat C[dir_num];
+	int c_data_type = CV_32FC1;
 
 	for (int i = 0; i < dir_num; i++)
 	{
-		C[i] = Mat::zeros(img_s, img_dpt);
+		C[i] = Mat::zeros(img_s, c_data_type);
 	}
 	// Get the magnitude map Cs for all directions
 	for (int i = 0; i < dir_num; i++)
@@ -95,36 +101,33 @@ int main(int argc, char** argv)
 		{
 			if (indices.at<char>(j) == i)
 			{
-				C[i].at<char>(j) = grad.at<char>(j);
+				C[i].at<float>(j) = grad.at<float>(j);
 			}
 		}
 	}
 	
 	//// Line shaping
 	Mat Spn[dir_num];
+	int sp_dpt = CV_32F;
+
 	for (int i = 0; i < dir_num; i++)
 	{
-		rot_mat = getRotationMatrix2D(Point2f(ks, ks), (i - 1) * 180 / dir_num, 1.0);
+		rot_mat = getRotationMatrix2D(Point2f((float)ks, (float)ks), 
+			(float)i * 180.0 / (float)dir_num, 1.0);
 		warpAffine(ker_ref, ker_real, rot_mat, ker_ref.size());
-		filter2D(C[i], Spn[i], C[i].depth(), ker_real);
+		filter2D(C[i], Spn[i], sp_dpt, ker_real);
 	}
 
 	// Sum the result
 	Mat Sp = Spn[0];
-	for (unsigned int i = 0; i < img_h * img_w; i++)
+	for (int i = 1; i < dir_num; i++)
 	{
-		for (int j = 1; j < dir_num; j++)
-		{
-			Sp.at<char>(i) += Spn[j].at<char>(i);
-		}
+		Sp += Spn[i];
 	}
 
-	// Map to [0, 1]
-	Mat S;
-	Sp.convertTo(Sp, CV_32FC1);
-	//normalize(Sp, S, 0.0, 1.0);
-	//namedWindow("median", WINDOW_AUTOSIZE);
-	S = 1 - Sp;
+	Sp.convertTo(Sp, CV_32FC1, 0.3);
+	Mat S = 1 - Sp;
+
 	imshow("display", S);
 	waitKey(0);
 
